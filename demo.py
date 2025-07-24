b9610a5207e01ed04927687e31165dbc159ef105
Jp4357 <jyotiprakash4357@gmail.com>
2025-07-24 14:20:33 +0530

    added data ingestion

--- demo.py
+++ demo.py
@@ -8,12 +8,17 @@
 # logging.critical("This is a critical message")
 
 
-from src.exception import MyException
-from src.logger import logging
-import sys
+# from src.exception import MyException
+# from src.logger import logging
+# import sys
 
-try:
-    a = 1 / 0
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys)
+# try:
+#     a = 1 / 0
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys)
+
+from src.pipline import training_pipeline
+
+pipeline = training_pipeline.TrainPipeline()
+pipeline.run_pipeline()
