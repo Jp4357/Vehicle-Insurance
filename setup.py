f0ba1bdfb1fc39d6d5c279fd6220e3ba68143f8d
Jp4357 <jyotiprakash4357@gmail.com>
2025-07-24 12:41:51 +0530

    added folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jyotiprakash",
+    author_email="jyotiprakash4357@gmail.com",
+    packages=find_packages(),
+)
